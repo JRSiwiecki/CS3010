1ab512ad8b26dc806f17e8a650a6ef62ee8ec677
JRSiwiecki <JRSiwiecki@gmail.com>
2022-10-08 15:07:28 -0700

    Get user input for # of linear equations

--- Project 1/Project1_jsiwiecki.java
+++ Project 1/Project1_jsiwiecki.java
@@ -1,7 +1,48 @@
-public class Project1_jsiwiecki 
+/*
+ * Name: Joseph Siwiecki
+ * Assignment: Programming Project 1
+ * Class: CS 3010.01
+ * Date: 10/7/22
+ */
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+ public class Project1_jsiwiecki 
 {
-    public static void main(String[] args) 
+    private static Scanner input = new Scanner(System.in);
+    private static int numberOfEquations;
+    private static double[][] augmentedMatrix;
+    
+    public static void main(String[] args)
     {
-        System.out.println("Hi");
+        getNumberOfEquations();
+    }
+
+    private static void getNumberOfEquations() 
+    {
+        boolean valid = false;
+
+        while (!valid) 
+        {
+            System.out.print("# of linear equations to solve: ");
+            int tempNum = input.nextInt();
+
+            if (tempNum > 0 && tempNum <= 10) 
+            {
+                numberOfEquations = tempNum;
+                valid = true;
+            }
+
+            else
+            {
+                System.out.println("Please enter a valid integer. [0 < numberOfEquations <= 10]");
+            }  
+        }
+    }
+
+    private static void addEquationsToArray() 
+    {
+
     }
 }

11f48baf8beed67c9002b2adeedb7a5a4d976ec7
JRSiwiecki <JRSiwiecki@gmail.com>
2022-10-09 16:01:43 -0700

    Update comments and cleanup code.

--- Project 1/Project1_jsiwiecki.java
+++ Project 1/Project1_jsiwiecki.java
@@ -15,6 +15,11 @@ public class Project1_jsiwiecki
      * and uses Gaussian Elimination with Scaled Partial Pivoting
      * to solve the system. The intermediate steps are displayed,
      * and the solution is displayed.
+     * 
+     * Using Gaussian Elimination with Scaled Partial Pivoting will be
+     * more accurate and precise than using Naive Gaussian Elimination or even
+     * just standard Partial Pivoting.
+     * 
      * @param input             Used to gather user input.
      * @param numberOfEquations Number of equations in the augmented matrix.
      * @param augmentedMatrix   Augmented matrix that stores user input for
@@ -76,6 +81,10 @@ public class Project1_jsiwiecki
         
         System.out.print("Enter 1 to input values using CLI, enter 2 to input values using file input: ");
 
+        /*
+         * Create two more Scanners because using the standard input scanner for the whole class
+         * does not operate properly. All scanners will be closed to prevent memory leaks.
+         */
         Scanner eqInput = new Scanner(System.in);
         Scanner file = new Scanner(System.in);
 
@@ -88,9 +97,10 @@ public class Project1_jsiwiecki
                 while (augmentedMatrix.length > numsEntered) 
                 {
                     System.out.println("Enter " + (augmentedMatrix[0].length) + " values for row " + (numsEntered + 1) + ": ");
-
                     eqInput = new Scanner(System.in);
+                    
                     String input = eqInput.nextLine();
+                    
                     String[] stringArray = input.split(" ");
 
                     for (int i = 0; i < (augmentedMatrix[0].length); i++) 
@@ -108,6 +118,11 @@ public class Project1_jsiwiecki
 
             } 
             
+            /*
+             * For file input, the entire file path must be specified for the file to read properly.
+             * In your IDE, right click on the file to get its file path, and then use right click again 
+             * but on the terminal instead when prompted for the file path.
+             */
             else if (choice == 2) 
             {
                 String filePath = "";
@@ -171,6 +186,9 @@ public class Project1_jsiwiecki
      */
     private static void gaussianEliminationWithScaledPartialPivoting()
     {
+        /*
+         * Find highest pivot value to prepare for swapping a row to work with partial pivoting.
+         */
         int maxLength = (Math.min(augmentedMatrix.length, augmentedMatrix[0].length));
         for (int pivot = 0; pivot < maxLength; pivot++)
         {
@@ -184,10 +202,17 @@ public class Project1_jsiwiecki
                 }
             }
             
+            /*
+             * Swap array rows based on pivot values.
+             */
             double[] swapArray = augmentedMatrix[pivot];
             augmentedMatrix[pivot] = augmentedMatrix[maxCoefficient];
             augmentedMatrix[maxCoefficient] = swapArray;
 
+            /*
+             * Calculate the scaled value for each array and convert augmentedMatrix' values
+             * to be consistent with the scaled value.
+             */
             for (int i = (pivot + 1); i < augmentedMatrix.length; i++)
             {
                 double scaledCoefficient = augmentedMatrix[i][pivot] / augmentedMatrix[pivot][pivot];
@@ -196,9 +221,11 @@ public class Project1_jsiwiecki
                 {
                     augmentedMatrix[i][j] = augmentedMatrix[i][j] - (scaledCoefficient * augmentedMatrix[pivot][j]);
                 }
+                
                 System.out.println("\n------------- INTERMEDIATE STEP -------------\n");
                 printAugmentedMatrix();
                 System.out.println("---------------------------------------------\n");
+
             }
         }
     }
@@ -208,6 +235,9 @@ public class Project1_jsiwiecki
      */
     private static void printAnswerArray()
     {
+        /*
+         * Calculate the value of the variables and place them in the answer array.
+         */
         double[] answerArray = new double[augmentedMatrix[0].length];
 
         int minLength = (Math.min(augmentedMatrix[0].length - 1, augmentedMatrix.length - 1));
@@ -222,19 +252,17 @@ public class Project1_jsiwiecki
             }
 
             answerArray[i] = (augmentedMatrix[i][augmentedMatrix.length] - sum) / augmentedMatrix[i][i];
-
-            if (Double.isNaN(answerArray[i]))
-            {
-                System.out.println("ERROR: Matrix is unsolveable.");
-                return;
-            }
         }
 
+        /*
+         * Print the answer array with up to 10 variables.
+         */
         char[] variables = { 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g' };
 
-        System.out.println("Solution: ");
+        System.out.println("Solved Varibles: ");
 
-        for (int i = 0; i < augmentedMatrix.length; i++) {
+        for (int i = 0; i < augmentedMatrix.length; i++) 
+        {
             System.out.print(variables[i] + " = " + answerArray[i] + "\n");
         }
 

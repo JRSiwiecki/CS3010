c739f1340d15cc18168c9cad9e3655540dfa7154
JRSiwiecki <JRSiwiecki@gmail.com>
2022-11-13 20:49:09 -0800

    Project 3 initial

---
+++ Project 3/src/Project3_jsiwiecki.java
@@ -0,0 +1,34 @@
+
+/**
+ * @author Joseph Siwiecki
+ *         Assignment: Programming Project 2
+ *         Class: CS 3010.01
+ *         Date: 11/19/22
+ */
+public class Project3_jsiwiecki 
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    private static void bisectionMethod()
+    {
+
+    }
+
+    private static void newtonRaphsonMethod() 
+    {
+
+    }
+
+    private static void secantMethod() 
+    {
+
+    }
+
+    private static void falsePositionMethod() 
+    {
+
+    }
+}

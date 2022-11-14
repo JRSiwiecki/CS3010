6cfcda532c4c95c6db9685d2eee8206693b892f8
JRSiwiecki <JRSiwiecki@gmail.com>
2022-11-13 21:12:21 -0800

    Boiler plate class variables + easier function calls

--- Project 3/src/Project3_jsiwiecki.java
+++ Project 3/src/Project3_jsiwiecki.java
@@ -7,14 +7,31 @@
  */
 public class Project3_jsiwiecki 
 {
+    private static final int MAX_ITERATIONS = 100;
+    private static double delta = 0.01;
+    private static double error = 0.01;
+    private static int counter = 0;
+    private static double c = 0.0;
+    private static double previousC = 0.0;
+    
     public static void main(String[] args)
     {
 
     }
 
+    private static double functionA(double x)
+    {
+        return ((2.0 * Math.pow(x, 3)) - (11.7 * Math.pow(x, 2)) + (17.7 * x) - 5);
+    }
+
+    private static double functionB(double x)
+    {
+        return (x + 10 - (x * Math.cosh((50.0 / x))));
+    }
+    
     private static void bisectionMethod()
     {
-
+        
     }
 
     private static void newtonRaphsonMethod() 

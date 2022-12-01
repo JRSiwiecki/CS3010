7aca45c1aef62938fe3957144446314e71b9b319
JRSiwiecki <JRSiwiecki@gmail.com>
2022-11-30 20:43:34 -0800

    Get file name

---
+++ Project 4/src/Project4_jsiwiecki.java
@@ -0,0 +1,24 @@
+/**
+ * @author Joseph Siwiecki
+ *         Assignment: Programming Project 4
+ *         Class: CS 3010.01
+ *         Date: 12/4/22
+ */
+
+import java.util.Scanner;
+
+public class Project4_jsiwiecki
+{
+    
+    
+    public static void main(String[] args) 
+    {
+        String fileName = "";
+
+        System.out.print("Enter the file name (with extension): ");
+        Scanner input = new Scanner(System.in);
+        fileName = input.nextLine() + ".txt";
+
+        
+    }
+}

8cfda55c309f1477dfc201ca2462e82497c8d041
JRSiwiecki <JRSiwiecki@gmail.com>
2022-11-30 20:49:49 -0800

    Open file + add test data

--- Project 4/src/Project4_jsiwiecki.java
+++ Project 4/src/Project4_jsiwiecki.java
@@ -5,13 +5,17 @@
  *         Date: 12/4/22
  */
 
+
+import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.DataInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Project4_jsiwiecki
 {
-    
-    
-    public static void main(String[] args) 
+    public static void main(String[] args) throws FileNotFoundException 
     {
         String fileName = "";
 
@@ -19,6 +23,13 @@ public class Project4_jsiwiecki
         Scanner input = new Scanner(System.in);
         fileName = input.nextLine() + ".txt";
 
-        
+        getXValues(fileName);
+    }
+
+    public static void getXValues(String fileName) throws FileNotFoundException
+    {
+        FileInputStream dataFile = new FileInputStream(fileName);
+        DataInputStream dataInput = new DataInputStream(dataFile);
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(dataInput));
     }
 }
